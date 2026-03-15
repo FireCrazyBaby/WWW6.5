470b1d8f931f9a540e8763c17d9a15027374f065
FireCrazyBaby <yourhotbabyonfire@gmail.com>
2026-03-15 13:24:38 +0700

    Create PremiumDepositBox.sol

---
+++ fireCrazy/Day14_SafeDepositSystem/PremiumDepositBox.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./BaseDepositBox.sol"; // 导入地基
+
+contract PremiumDepositBox is BaseDepositBox {
+    // ... 这里放 PremiumDepositBox 的代码 ...
+}
