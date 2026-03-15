354f237047c29b8f89563ba8e932bb069178eecf
FireCrazyBaby <yourhotbabyonfire@gmail.com>
2026-03-15 14:22:22 +0700

    Update day14_IDepositBox.sol

--- fireCrazy/Day14_SafeDepositSystem/day14_IDepositBox.sol
+++ fireCrazy/Day14_SafeDepositSystem/day14_IDepositBox.sol
@@ -6,6 +6,9 @@ interface IDepositBox {
     function transferOwnership(address newOwner) external;
     function storeSecret(string calldata secret) external;
     function getSecret() external view returns (string memory);
+    
+    // 【修复点】：这里严格规定了 getBoxType 必须带 pure
     function getBoxType() external pure returns (string memory);
+    
     function getDepositTime() external view returns (uint256);
 }
