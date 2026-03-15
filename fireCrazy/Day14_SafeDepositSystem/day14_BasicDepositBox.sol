f896185354d8c62645f7febb4bd77d272dc4f8c8
FireCrazyBaby <yourhotbabyonfire@gmail.com>
2026-03-15 14:34:09 +0700

    Update day14_BasicDepositBox.sol

--- fireCrazy/Day14_SafeDepositSystem/day14_BasicDepositBox.sol
+++ fireCrazy/Day14_SafeDepositSystem/day14_BasicDepositBox.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 import "./day14_BaseDepositBox.sol"; // 导入地基
 
 contract BasicDepositBox is BaseDepositBox {
-    // ... 这里放 BasicDepositBox 的代码 ...
+    function getBoxType() external pure override returns (string memory) {
+        return "Basic";
+    }
 }
